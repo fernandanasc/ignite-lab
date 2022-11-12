9b4646a070a2f3bf7feb263f03abc05bbb25773e
fernandanasc <fernandavitnasc@gmail.com>
2022-11-11 21:21:48 -0300

    final
    Criação da interface com os componentes

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,15 +1,18 @@
+import { ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
-import { ReactNode } from 'react';
+
 
 export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode;
     asChild?: boolean;
+    className?: string; 
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
     const Comp = asChild ? Slot : 'span';
+    
     return (
         <Comp
             className={clsx(
@@ -18,7 +21,8 @@ export function Text({ size = 'md', children, asChild }: TextProps) {
                     'text-xs': size === 'sm',
                     'text-sm': size === 'md',
                     'text-md': size === 'lg',
-                }
+                },
+                className,
             )}
         >
             {children}
